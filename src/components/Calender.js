0a0c3cfd8419adcf912c4fcc074ca1294d009158
mohsen <mohsen1299@gmail.com>
2020-12-29 23:41:41 +0330

    show two month nad some css two show better

--- src/components/Calender.js
+++ src/components/Calender.js
@@ -1,9 +1,30 @@
 import React from "react";
+import jalaali from "jalaali-js";
 
 import MonthCalender from "./MonthCalender";
 
 const Calender = () => {
-  return <MonthCalender />;
+  const jToday = jalaali.toJalaali(new Date());
+
+  return (
+    <div
+      style={{
+        direction: "rtl",
+        display: "flex",
+        justifyContent: "space-around",
+      }}
+    >
+      <span>&lt;&lt;</span>
+      <MonthCalender
+        year={jToday.jy}
+        month={jToday.jm}
+        position={0}
+        showFullDayTitle={true}
+      />
+      <MonthCalender year={jToday.jy} month={jToday.jm + 1} position={1} />
+      <span>&gt;&gt;</span>
+    </div>
+  );
 };
 
 export default Calender;
