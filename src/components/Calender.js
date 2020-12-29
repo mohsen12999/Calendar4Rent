821f182614d6b556823ffd24857cb1db16004c02
mohsen <mohsen1299@gmail.com>
2020-12-29 23:59:53 +0330

    increase and decrease month button

--- src/components/Calender.js
+++ src/components/Calender.js
@@ -6,6 +6,29 @@ import MonthCalender from "./MonthCalender";
 const Calender = () => {
   const jToday = jalaali.toJalaali(new Date());
 
+  const [currentYear, setCurrentYear] = React.useState(jToday.jy);
+  const [currentMonth, setCurrentMonth] = React.useState(jToday.jm);
+
+  const increaseMonth = () => {
+    const newMonth = currentMonth + 1;
+    if (newMonth > 12) {
+      setCurrentMonth(1);
+      setCurrentYear(currentYear + 1);
+    } else {
+      setCurrentMonth(newMonth);
+    }
+  };
+
+  const decreaseMonth = () => {
+    const newMonth = currentMonth - 1;
+    if (newMonth < 1) {
+      setCurrentMonth(12);
+      setCurrentYear(currentYear - 1);
+    } else {
+      setCurrentMonth(newMonth);
+    }
+  };
+
   return (
     <div
       style={{
@@ -14,15 +37,23 @@ const Calender = () => {
         justifyContent: "space-around",
       }}
     >
-      <span>&lt;&lt;</span>
+      <span onClick={() => decreaseMonth()} style={{ cursor: "pointer" }}>
+        &lt;&lt;
+      </span>
       <MonthCalender
-        year={jToday.jy}
-        month={jToday.jm}
+        year={currentYear}
+        month={currentMonth}
         position={0}
         showFullDayTitle={true}
       />
-      <MonthCalender year={jToday.jy} month={jToday.jm + 1} position={1} />
-      <span>&gt;&gt;</span>
+      <MonthCalender
+        year={currentMonth === 12 ? currentYear + 1 : currentYear}
+        month={currentMonth === 12 ? 1 : currentMonth + 1}
+        position={1}
+      />
+      <span onClick={() => increaseMonth()} style={{ cursor: "pointer" }}>
+        &gt;&gt;
+      </span>
     </div>
   );
 };
